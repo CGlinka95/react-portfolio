ee1c785e08c37b97a7cddcdceea106c5c09faa9b
Christopher Glinka <glinkachris@gmail.com>
2022-11-03 13:59:46 -0600

    Header
    Working on finishing the header. I have the image, buttons, titles, and header social links finished. All that is left is the scroll down button

---
+++ react-portfolio-website-1/src/components/header/CTA.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+//import CV from "../../assets/cv.pdf"
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href="#" className='btn'>Download CV</a>
+        {/* <a href={CV} download>Download CV</a>*/}
+        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
--- react-portfolio-website-1/src/components/header/Header.jsx
+++ react-portfolio-website-1/src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./header.css"
+import CTA from "./CTA"
+import ME from "../../assets/selfie.png"
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Christopher Glinka</h1>
+        <h5 className='text-light'>Jr. Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className='me'>
+          <img src={ME} alt="Photo of me, the creator" title='Photo of me, the creator' />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
