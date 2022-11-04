dd4fde6411e2de28f4f72e41f897a40bfac35143
Christopher Glinka <glinkachris@gmail.com>
2022-11-04 15:36:01 -0600

    Nav and Parallax
    Finished the floating nav bar and parallax scroll effect. Positioned the scrolldown button in the correct spot.

--- react-portfolio-website-1/src/components/nav/Nav.jsx
+++ react-portfolio-website-1/src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react'
+
 import React from 'react'
 import "./nav.css"
+import { AiOutlineHome, AiOutlineUser, AiOutlineMessage } from 'react-icons/ai'
+import { BiBook } from 'react-icons/bi'
+import { RiServiceLine } from 'react-icons/ri'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
+
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav("#")} className={activeNav === "#" ? "active" : ""}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? "active" : ""}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNav("#experience")} className={activeNav === "#experience" ? "active" : ""}><BiBook /></a>
+      <a href="#services" onClick={() => setActiveNav("#services")} className={activeNav === "#services" ? "active" : ""}><RiServiceLine /></a>
+      <a href="#contact" onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? "active" : ""}><AiOutlineMessage /></a>
+    </nav>
   )
 }
 
