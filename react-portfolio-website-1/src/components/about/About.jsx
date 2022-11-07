8261b9648398b9dbae47a8926dbe74a423ef5ee4
Christopher Glinka <glinkachris@gmail.com>
2022-11-06 17:58:04 -0700

    About and Experience
    Finished the about and experience components of the website.

--- react-portfolio-website-1/src/components/about/About.jsx
+++ react-portfolio-website-1/src/components/about/About.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import "./about.css"
+import ME from "../../assets/me.jpg"
+import { FaAward } from "react-icons/fa"
+import { FiUsers } from "react-icons/fi"
+import { VscFolderLibrary} from "react-icons/vsc"
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className="about__me">
+          <div className='about__me-image'>
+            <img src={ME} alt="Photo of me, the creator" title="Photo of me, the creator" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Technical Education</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>.....................</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>10+ Completed Projects</small>
+            </article>
+          </div>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, nisi similique necessitatibus recusandae rerum adipisci vel doloremque, voluptatem aspernatur quasi quo dicta incidunt, officia ipsum sit assumenda esse tempora! Est!</p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
